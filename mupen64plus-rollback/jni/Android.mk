3f05b1679c76787848ef8b32effa90973c1ebe3d
Rollback Builder <rollback@mupen64plus.com>
2026-08-06 02:43:47 +0000

    Fix: Include mupen64plus-core.mk in rollback Android.mk for ndk-build

--- mupen64plus-rollback/jni/Android.mk
+++ mupen64plus-rollback/jni/Android.mk
@@ -30,6 +30,7 @@ include $(BUILD_STATIC_LIBRARY)
 ###########################
 # Rollback JNI bridge
 ###########################
+include $(LOCAL_PATH)/../mupen64plus-core/mupen64plus-core.mk
 include $(CLEAR_VARS)
 LOCAL_MODULE := mupen64plus-rollback
 
