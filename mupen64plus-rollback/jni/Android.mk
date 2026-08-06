6157e1871639d13bef630c91a8deada0ab9ed905
Rollback Builder <rollback@mupen64plus.com>
2026-08-06 02:55:33 +0000

    Fix: Include native_common.mk in rollback Android.mk for library deps

--- mupen64plus-rollback/jni/Android.mk
+++ mupen64plus-rollback/jni/Android.mk
@@ -31,6 +31,7 @@ include $(BUILD_STATIC_LIBRARY)
 # Rollback JNI bridge
 ###########################
 JNI_LOCAL_PATH := $(LOCAL_PATH)/../mupen64plus-core
+include $(LOCAL_PATH)/../build_common/native_common.mk
 include $(LOCAL_PATH)/../mupen64plus-core/mupen64plus-core.mk
 include $(CLEAR_VARS)
 LOCAL_MODULE := mupen64plus-rollback
